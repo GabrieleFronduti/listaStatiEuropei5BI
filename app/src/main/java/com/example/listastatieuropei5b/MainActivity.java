e730a51d8a77594b59925d3529f293fed60ee2e1
GabrieleFronduti <gabriele.fronduti17@gmail.com>
2021-12-03 16:04:21 +0100

    parte 2

--- app/src/main/java/com/example/listastatieuropei5b/MainActivity.java
+++ app/src/main/java/com/example/listastatieuropei5b/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.listastatieuropei5b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lwStati;
+    String listaStati[] = {"Italia", "Francia", "Germania", "Lussemburgo", "Svizzera"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lwStati = (ListView)findViewById(R.id.listaStati);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,R.layout.activity_main,R.id.textView, listaStati);
+        lwStati.setAdapter(aa);
     }
 }
